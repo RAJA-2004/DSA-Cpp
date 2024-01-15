a17123228d2903bd45ed01e650b493b4d024360f
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-01-15 21:08:49 +0530

    added commonElement3sortedArray.cpp

---
+++ Array/assignment/commonElement3sortedArray.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+// SET APPROACH
+
+vector<int>common(vector<int>& arr1,vector<int>& arr2,vector<int>& arr3,int n1,int n2, int n3){
+    set<int> st;
+    int i,j,k;
+    i=j=k=0;
+    vector<int> ans;
+    while(i<n1 && j<n2 && k<n3){
+        if(arr1[i]==arr2[j] && arr2[j]==arr3[k]){
+            st.insert(arr1[i]);
+            i++;
+            j++;
+            k++;
+        }
+        else if(arr1[i]<arr2[j]){
+            i++;
+        }
+        else if(arr2[j]<arr3[k]){
+            j++;
+        }
+        else{
+            k++;
+        }
+    }
+    for(auto values:st){
+        ans.push_back(values);
+    }
+    return ans;
+}
+
+vector<int> duplicate(vector<int>& a){
+    vector<int> ans;
+    sort(a.begin(),a.end());
+    for(int i=0;i<a.size();i++){
+        for(int j=i+1;j<a.size();j++){
+            if(a[i]==a[j]){
+                a[j]=INT_MIN;
+            }
+        }
+    }
+    for(int i=0;i<a.size();i++){
+        if(a[i]!=INT_MIN){
+            ans.push_back(a[i]);
+        }
+    }
+    return ans;
+}
+
+vector<int>common1(vector<int>& arr1,vector<int>& arr2,vector<int>& arr3,int n1,int n2, int n3){
+    // REMOVE DUPLICATES APPROACH SO THAT NO ADDITIONAL DS IS REQUIRED
+    arr1 = duplicate(arr1);
+    arr2 = duplicate(arr2);
+    arr3 = duplicate(arr3);
+    int i,j,k;
+    i=j=k=0;
+    vector<int> ans;
+    while(i<n1 && j<n2 && k<n3){
+        if(arr1[i]==arr2[j] && arr2[j]==arr3[k]){
+            ans.push_back(arr1[i]);
+            i++;
+            j++;
+            k++;
+        }
+        else if(arr1[i]<arr2[j]){
+            i++;
+        }
+        else if(arr2[j]<arr3[k]){
+            j++;
+        }
+        else{
+            k++;
+        }
+    }
+    return ans;
+}
+int main(){
+    vector<int>arr1 {3,3,3};
+    vector<int>arr2 {3,3,3};        
+    vector<int>arr3 {3,3,3};
+
+    int n1 = arr1.size();
+    int n2 = arr2.size();
+    int n3 = arr3.size();
+    
+    vector<int> res;
+    vector<int> res2;
+    res=common(arr1,arr2,arr3,n1,n2,n3);
+
+    res2=common1(arr1,arr2,arr3,n1,n2,n3);
+    for(auto values : res){
+        cout << values << " ";
+    }
+    cout << endl;
+    for(auto values : res2){
+        cout << values << " ";
+    }
+    return 0;
+}
