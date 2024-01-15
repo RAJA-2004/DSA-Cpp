c848b61f903f45d9676d33b4da1b02ef46e1b133
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-01-15 21:08:09 +0530

    Update firstRepeatingElement.cpp

--- Array/assignment/firstRepeatingElement.cpp
+++ Array/assignment/firstRepeatingElement.cpp
@@ -14,9 +14,32 @@ int firstRepeat(vector<int>& arr,int n){
     }
     return -1;
 }
+int noWithoutHash(vector<int>& arr,int n){
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+    }
+    vector<int> ans(max+1);
+    for(int i=0;i<=max;i++){
+        ans[i]=i;
+    }
+    for(int i=0;i<n;i++){
+        ans[arr[i]]++;
+    }
+    for(int i=0;i<n;i++){
+        if(ans[arr[i]]>=2){
+            return arr[i];
+        }
+    }
+    return -1;
+}
 int main(){
-    vector<int> arr{1,2,3,2,3,4};
+    vector<int> arr{1,2,3,4};
     int n = arr.size();
     cout << "first repeating element is on index : " << firstRepeat(arr,n) << endl;
+
+    cout << "first repeating element is on index : " << noWithoutHash(arr,n) << endl;
     return 0;
 }
