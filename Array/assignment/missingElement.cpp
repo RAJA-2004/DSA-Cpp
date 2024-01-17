9e5a605146afd6dd6a2223cd0e56c650cf385b20
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-01-17 11:15:36 +0530

    Update missingElement.cpp

--- Array/assignment/missingElement.cpp
+++ Array/assignment/missingElement.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int main(){
     vector<int>nums {1,0,3,4}; // 0134
 
     // SORT AND CHECK INDEX APPROACH
     sort(nums.begin(),nums.end());
-    for(int i=0;i<nums.size();i++){
+    for(int i=0;i<nums.size()-1;i++){
         if((nums[i]+1)!=nums[i+1]){
             cout << "missing element : " << i+1 << endl;
         }
     }
+
+    // BINARY SEARCH APPROACH
+    sort(nums.begin(),nums.end());
+    int x = -1;
+    for(int i=0;i<nums.size();i++){
+        if(!(binary_search(nums.begin(),nums.end(),i+1))){
+            x = i+1;
+        }
+    }
+    cout << "missing element : " << x << endl;
 
     // VISITED APPROACH
     // for(int i=0;i<nums.size();i++){
