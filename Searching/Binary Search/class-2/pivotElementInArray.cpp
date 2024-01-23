64ef13e374570e73bf4fef1032d12e852b2bc66f
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-01-23 14:19:37 +0530

    Update pivotElementInArray.cpp

--- Searching/Binary Search/class-2/pivotElementInArray.cpp
+++ Searching/Binary Search/class-2/pivotElementInArray.cpp
@@ -2,13 +2,17 @@
 #include <vector>
 using namespace std;
 
+// largest element will be the answer
 // first increase then pivot then sudden fall then again increase
 
 int pivot(vector<int>ans){
     int start = 0;
     int end = ans.size()-1;
     int mid = start + (end-start)/2;
-    while(start<end){
+    while(start<=end){
+        if(start==end){
+            return start;
+        }
         if(mid+1<=ans.size() && ans[mid]>ans[mid+1]){
             return mid;
         }
@@ -16,18 +20,22 @@ int pivot(vector<int>ans){
             return mid-1;
         }
 
-        if(ans[start]>=ans[mid]){
+        if(ans[start]>ans[mid]){
+            // pivot element on first line
+            // left search
             end = mid - 1;
         }
         else{
-            start = mid;
+            // pivot element on second line
+            // right search & mid can also be the answer
+            start = mid+1;
         }
         mid = start + (end-start)/2;
     }
-    return start;
+    return -1;
 }
 int main(){
-    vector<int>arr{7,7,7,7,1,2};
+    vector<int>arr{8,9,10,5,6};
     int x = pivot(arr);
     cout << x << endl;
     return 0;
