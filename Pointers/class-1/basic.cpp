e63f2e1fcd76059387e8fde0a01e950d5939e5f5
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-02-01 22:44:05 +0530

    Update basic.cpp

--- Pointers/class-1/basic.cpp
+++ Pointers/class-1/basic.cpp
@@ -6,10 +6,39 @@
 #include <map>
 using namespace std;
 
+void size(int arr[]){
+    cout << "size inside function : " << sizeof(arr) << endl;
+}
+
+void update(int* p){
+    cout << "size of inside update function : " << sizeof(p) << endl;
+    cout << *p+5 << endl;
+}
+
+void values(int* p,int* q){
+    *p = 100;
+    *q = 200;
+}
+
 int main(){
     int a = 5;
     int* ptr = &a;
     int** p = &ptr;
+    int arr[10] = {1,2,3,4};
+
+    cout << "actual size of arr : " << sizeof(arr) << endl;
+    size(arr);
+    update(&a);
+
+    int* p1 = &arr[1];
+    int* p2 = &arr[2];
+
+    values(p1,p2);
+
+    for(auto values : arr){
+        cout << values << " ";
+    }
+    cout << endl;
 
     cout << "value of a    : " << a << endl;
     cout << "value of ptr  : " << ptr << endl;
