7cb3b918156be4f805e498c943494ba33deadd85
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-02-01 22:44:03 +0530

    Update basic.cpp

--- Pointers/class-2/basic.cpp
+++ Pointers/class-2/basic.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main(){
     int arr[10] = {1,2,3,4,5,6,7,8,9,10};
-
+    
     cout << "    arr  : " << arr << endl;
     cout << "& arr[0] : " << &arr[0] << endl;
     cout << "   & arr : " << &arr << endl;
