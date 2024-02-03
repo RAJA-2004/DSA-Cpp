1b5bbc738e2f6c6db6f057a9c19705b41fa81e79
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-02-04 04:59:42 +0530

    Create basic.cpp

---
+++ Recursion/class-1/basic.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <map>
+using namespace std;
+
+int fact(int n){
+    // base case
+    if(n<=1){
+        return 1;
+    }
+    // recursion : function calling itself
+    return n*fact(n-1);
+}
+
+int main(){
+    int a = 5;
+    int b = fact(a);
+    cout << b << endl;
+    return 0;
+}
