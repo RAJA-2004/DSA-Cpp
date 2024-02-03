911b9671c5ceb38cbea9010775ac4fb91151a67b
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-02-04 04:59:48 +0530

    Create fibonacci.cpp

---
+++ Recursion/class-1/fibonacci.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <map>
+using namespace std;
+
+int fibo(int n){
+    // base case
+    if(n==1){
+        return 0;
+    }
+    if(n==2){
+        return 1;
+    }
+    // recursion
+    return fibo(n-1)+fibo(n-2);
+}
+
+int main(){
+
+    // 0 1 1 2 3 5 8 13 21 ....
+
+    int a = 5;
+    int c = fibo(a);
+    cout << a << " term is : " << c << endl;
+
+    return 0;
+}
