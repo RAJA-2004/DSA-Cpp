a5834a55a3e18b923518f2ab9298453ad5bd2ef0
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-02-05 19:33:28 +0530

    Update printArray.cpp

--- Recursion/class-2/printArray.cpp
+++ Recursion/class-2/printArray.cpp
@@ -5,6 +5,7 @@
 #include <set>
 #include <map>
 using namespace std;
+
 void print(int arr[],int n,int i){
     // base case
     if(i>=n){
@@ -17,9 +18,19 @@ void print(int arr[],int n,int i){
 
     cout << arr[i] << " ";
 }
+
+void printArray(int arr[],int n){
+    if(n==0){
+        return;
+    }
+    cout << *arr << " ";
+    printArray(arr+1,n-1);
+}
 int main(){
     int arr[5]={1,2,3,4,5};
     int i = 0;
     print(arr,5,i);
+    cout << endl;
+    printArray(arr,5);
     return 0;
 }
