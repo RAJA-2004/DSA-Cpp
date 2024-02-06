e8472c8b5ca6c2b83b2e19ec4d059341b8e8109d
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-02-06 17:02:40 +0530

    Create subSequenceOfString.cpp

---
+++ Recursion/class-3/subSequenceOfString.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <map>
+using namespace std;
+
+void sub(string& s,string t,int i,vector<string>& ans){
+    if(i>=s.length()){
+        // cout << t << endl;
+        // store the output
+        ans.push_back(t);
+        return;
+    }
+    // exclude
+    sub(s,t,i+1,ans);
+    // include 
+    t.push_back(s[i]);
+    sub(s,t,i+1,ans);
+} 
+
+
+
+int main(){
+    string a = "abc";
+    string t = "";
+    vector<string> ans;
+    int i = 0 ;
+    sub(a,t,i,ans);
+    for(auto val : ans){
+        cout << val << endl;
+    }
+    return 0;
+}
