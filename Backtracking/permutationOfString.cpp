42ade65df6211d7ab726ceb84d8f5c0f6ff110ff
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-02-11 17:52:12 +0530

    Create permutationOfString.cpp

---
+++ Backtracking/permutationOfString.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <map>
+using namespace std;
+
+void print(string& s,int i){
+    // base
+    if(i>=s.length()){
+        cout << s << " ";
+        return;
+    }
+    // processing
+    for(int j=i;j<s.length();j++){
+        swap(s[i],s[j]);
+        // recursion
+        print(s,i+1);
+        // backtracking -> why this -> to recreate the original string
+
+        // swap(s[i],s[j]);
+    }
+}
+
+int main(){
+    string a = "abc";
+    int i = 0;
+    print(a,i);
+    return 0;
+}
