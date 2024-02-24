f4e38e50109cdfe7b5cf768eae68cf9ced357f16
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-02-24 16:35:50 +0530

    Create preOrder.cpp

---
+++ Trees/class-1/Traversal/preOrder.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <queue>
+#include <map>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data=data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node* createTree(){
+    int data;
+    cout << "enter data : ";
+    cin >> data;
+    if(data==-1) return NULL;
+    Node* root = new Node(data);
+
+    cout << "enter data to left of : " << data << endl;
+    root->left = createTree();
+
+    cout << "enter data to right of : " << data << endl;
+    root->right = createTree();
+    return root;
+}
+
+// P L R
+void inOrder(Node* root){
+    if(root==NULL) return;
+    cout << root->data << " ";
+    inOrder(root->left);
+    inOrder(root->right);
+}
+
+
+int main(){
+    Node* root = createTree();
+    inOrder(root);
+    return 0;
+}
