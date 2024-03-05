d7a6558725359ca0edfd2d4e223259902f117196
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-03-05 19:01:42 +0530

    Create basicChar.cpp

---
+++ Strings/class-1/basicChar.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+#include <set>
+
+using namespace std;
+int main(){
+    char ch[20];
+    char arr[20];
+    cin >> arr; // do not read after space 
+    cin.getline(ch,50);
+    cout << ch << endl;
+    return 0;
+}
