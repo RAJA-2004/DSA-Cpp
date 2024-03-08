08bfd9f41a40610470fdaee1786901b133542795
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-03-08 17:16:23 +0530

    Create minValue.cpp

---
+++ Trees/Bst/class-1/minValue.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <set>
+#include <map>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data=data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// ! inorder of BST is always SORTED
+
+Node* bst(Node* root,int key){
+    if(root == NULL){
+        root = new Node(key);
+        return root;
+    }
+    if(root->data > key){
+        root->left = bst(root->left,key);
+    }
+    else{ 
+        root->right = bst(root->right,key);
+    }
+    return root;
+}
+
+void inorder(Node* root,vector<int>& ans){
+    if(root == NULL) return;
+    inorder(root->left,ans);
+    ans.push_back(root->data);
+    inorder(root->right,ans);
+}
+
+int main(){
+    Node* root = NULL;
+    vector<int> arr{100,90,80,95,200,150,250};
+    for(int i=0;i<arr.size();i++){
+        root = bst(root,arr[i]);
+    }
+    vector<int> ans;
+    inorder(root,ans);
+    cout << "min val : " << ans[0] << endl;
+    return 0;
+}
