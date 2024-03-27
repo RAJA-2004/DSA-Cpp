c7f444db7defdeca1b0a55ccffd2a2fefa388dc0
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-03-27 08:52:56 +0530

    Update largestBstInBinaryTree.cpp

--- Trees/Bst/class-3/largestBstInBinaryTree.cpp
+++ Trees/Bst/class-3/largestBstInBinaryTree.cpp
@@ -1,14 +1,8 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <set>
-#include <queue>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Node{
-    public:
+public:
     int data;
     Node* left;
     Node* right;
@@ -21,15 +15,12 @@ class Node{
 };
 
 class NodeData{
-    public:
+public:
     int minVal;
     int maxVal;
     int size;
     bool validBst;
-    NodeData(){
-
-    };
-    NodeData(int size,int max,int min,bool valid){
+    NodeData(int size, int max, int min, bool valid){
         this->size = size;
         minVal = min;
         maxVal = max;
@@ -37,28 +28,33 @@ class NodeData{
     }
 };
 
-NodeData* findLargestBst(Node* root,int& ans){
+NodeData* findLargestBst(Node* root, int& ans){
     // base case
     if(root == NULL){
-        NodeData* temp = new NodeData(0,INT_MIN,INT_MAX,true);
+        NodeData* temp = new NodeData(0, INT_MIN, INT_MAX, true);
         return temp;
     }
-    NodeData* leftAns = findLargestBst(root->left,ans);
-    NodeData* rightAns = findLargestBst(root->right,ans);
+    NodeData* leftAns = findLargestBst(root->left, ans);
+    NodeData* rightAns = findLargestBst(root->right, ans);
 
-    NodeData* currNode;
-    // left side + right side + node itself
-    currNode->size = leftAns->size+rightAns->size+1;
-    currNode->maxVal = max(root->data,rightAns->maxVal);
-    currNode->minVal = min(root->data,leftAns->minVal);
+    NodeData* currNode = new NodeData(0, 0, 0, false); // Allocate memory for currNode
 
-    if(leftAns->validBst && rightAns->validBst && (root->data < rightAns->minVal && root->data > leftAns->maxVal)){
-        currNode->validBst = true;
-    }else{
-        currNode->validBst = false;
-    }
-    if(currNode->validBst){
-        ans = max(ans,currNode->size);
+    // Check if leftAns and rightAns are not NULL before accessing their members
+    if (leftAns != NULL && rightAns != NULL) {
+        // left side + right side + node itself
+        currNode->size = leftAns->size + rightAns->size + 1;
+        currNode->maxVal = max(root->data, rightAns->maxVal);
+        currNode->minVal = min(root->data, leftAns->minVal);
+
+        if (leftAns->validBst && rightAns->validBst && (root->data < rightAns->minVal && root->data > leftAns->maxVal)){
+            currNode->validBst = true;
+        }
+        else{
+            currNode->validBst = false;
+        }
+        if (currNode->validBst){
+            ans = max(ans, currNode->size);
+        }
     }
     return currNode;
 }
@@ -80,7 +76,7 @@ int main(){
     Node* root = NULL;
     root = create();
     int ans = 0;
-    NodeData* temp = findLargestBst(root,ans);
-    cout << temp->size << endl;
+    NodeData* temp = findLargestBst(root, ans);
+    cout << ans << endl;
     return 0;
 }
