1c6ac69592a9eec3281358311c39f1a8d8937ef2
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-03-27 08:52:49 +0530

    Update boilerplate.cpp

--- boilerplate.cpp
+++ boilerplate.cpp
@@ -1,14 +1,18 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <set>
-#include <map>
-#include <queue>
-#include <stack>
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// #include <string>
+// #include <set>
+// #include <map>
+// #include <queue>
+// #include <stack>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define endl '\n'
 
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    
     return 0;
 }
