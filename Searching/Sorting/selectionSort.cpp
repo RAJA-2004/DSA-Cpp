e522e60c03b4da0c3c3569f9fde400c4192c32ff
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-04-04 16:10:10 +0530

    Update selectionSort.cpp

--- Searching/Sorting/selectionSort.cpp
+++ Searching/Sorting/selectionSort.cpp
@@ -2,20 +2,35 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-void ss(vector<int>& ans){
-    for(int i=0;i<=ans.size()-2;i++){
-        int min = i;
-        for(int j=i+1;j<=ans.size()-1;j++){
-            if(ans[j]<ans[min]){
-                min=j;
+// void ss(vector<int>& ans){
+//     for(int i=0;i<=ans.size()-2;i++){
+//         int min = i;
+//         for(int j=i+1;j<=ans.size()-1;j++){
+//             if(ans[j]<ans[min]){
+//                 min=j;
+//             }
+//         }
+//         swap(ans[i],ans[min]);
+//     }
+// }
+
+void selectionsort(vector<int>& ans){
+    int n = ans.size();
+    for(int i=0;i<n-1;i++){
+        int mini = i;
+        for(int j=i;j<n;j++){
+            if(ans[j]<ans[mini]){
+                mini = j;
             }
         }
-        swap(ans[i],ans[min]);
+        swap(ans[i],ans[mini]);
     }
 }
+
 int main(){
     vector<int>ans{2,4,5,1,3,0};
-    ss(ans);
+    // ss(ans);
+    selectionsort(ans);
     for(auto values : ans){
         cout << values << " ";
     }
