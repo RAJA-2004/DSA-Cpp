fbfe46e6dced3b9cfe8925073dd5198744de5879
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-04-21 17:37:41 +0530

    Create adjList.cpp

---
+++ Graphs/class-1/adjList.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    vector<int> adj[n+1];
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin >> u >> v;
+
+        // ! for undirected graph -> O(2*E)
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+
+        // ! for directed graph only one will be true -> O(E)
+        // adj[u].push_back(v);
+    }
+    return 0;
+}
