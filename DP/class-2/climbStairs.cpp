b2636c6898a4f924cc5e04e9a63e2f7c3ccf5f59
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-04-29 11:42:06 +0530

    Create climbStairs.cpp

---
+++ DP/class-2/climbStairs.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// <!-- time complexity  : O() -->
+// <!-- space complexity : O() -->
+
+int solve(int n){
+	
+	// if(n<=1) return 1;
+	// return solve(n-1)+solve(n-2);
+	
+	vector<int> dp(n+1,-1);
+	
+	// if(n<=1) return 1;
+	// if(dp[n]!=-1) return dp[n];
+	// return dp[n] = solve(n-1)+solve(n-2);
+	
+	dp[0] = 1;
+	dp[1] = 1;
+	for(int i=2;i<n+1;i++){
+		dp[i] = dp[i-1] + dp[i-2];
+	}
+	return dp[n];
+}
+
+int main(){
+	int n;
+	cin >> n;
+	cout << solve(n) << endl;
+	return 0;
+}
