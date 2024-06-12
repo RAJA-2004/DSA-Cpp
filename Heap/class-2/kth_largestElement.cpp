7b9927f9bb8185176947fc556b6202c5c58366ac
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-06-12 12:18:43 +0530

    Create kth_largestElement.cpp

---
+++ Heap/class-2/kth_largestElement.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+	int n,target;
+	cin >> n >> target;
+	vector<int> ans(n);
+	for(int i=0;i<n;i++){
+		cin >> ans[i];
+	}
+	
+	// <!-- MIN HEAP -->
+	
+	priority_queue<int, vector<int>, greater<int> > pq;
+	
+	for(int i=0;i<n;i++){
+		pq.push(ans[i]);
+		if(pq.size()>target){
+			pq.pop();
+		}
+	}
+	
+	cout << pq.top() << endl;
+	
+	return 0;
+}
